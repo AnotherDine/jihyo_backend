5a2c81cd9eb8ed31d1f6d5e1dfe33adad55fb308
kimiihvo <kimiihvo@gmail.com>
2024-02-07 11:46:45 +0900

    feat: CI/CD Set up

---
+++ src/main/java/com/anotherdine/jihyo_backend/JihyoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.anotherdine.jihyo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JihyoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JihyoBackendApplication.class, args);
+	}
+
+}
