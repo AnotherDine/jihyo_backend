fe7adaa18a2e999b98c56762ee86001f7459fdd6
kimiihvo <kimiihvo@gmail.com>
2024-02-15 20:27:02 +0900

    test: CORS Test

--- src/main/java/com/anotherdine/jihyo_backend/MyController.java
+++ src/main/java/com/anotherdine/jihyo_backend/MyController.java
@@ -1,11 +1,13 @@
 package com.anotherdine.jihyo_backend;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://jhkim-frontend-cicd-bucket.s3-website.ap-northeast-2.amazonaws.com/")
 public class MyController {
     @GetMapping("/data")
     public String getData() {
